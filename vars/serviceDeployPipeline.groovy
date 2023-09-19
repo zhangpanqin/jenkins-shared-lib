23fc63a099f4a3f78583e7c03bd01bb81422dd7e
zhangpanqin <zhangpanqin@outlook.com>
2023-09-19 15:55:18 +0800

    2

--- vars/serviceDeployPipeline.groovy
+++ vars/serviceDeployPipeline.groovy
@@ -2,12 +2,14 @@ import com.mflyyou.PipelineParam
 
 def call(PipelineParam config) {
     def script = this
-    agent { docker { image 'maven:3.9.4-eclipse-temurin-17-alpine' } }
-    stages {
-        stage('build') {
-            steps {
-                script.sh 'mvn --version'
-                script.echo "echo ${config.branchName}"
+    pipeline {
+        agent { docker { image 'maven:3.9.4-eclipse-temurin-17-alpine' } }
+        stages {
+            stage('build') {
+                steps {
+                    script.sh 'mvn --version'
+                    script.echo "echo ${config.branchName}"
+                }
             }
         }
     }
